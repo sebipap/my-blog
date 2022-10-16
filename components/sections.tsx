613e212d58ec6dcef6e849d03e0aca3cc6017dde
sebipapsu <sebipaps@gmail.com>
2022-10-15 22:08:16 -0300

    fix: remove variable composed styles as they are not available in tailwind.

--- components/sections.tsx
+++ components/sections.tsx
@@ -1,22 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const Block = ({
-  width,
-  height,
   children,
-  color,
+  styles,
   onClick,
   emoji,
 }: {
-  width: number;
-  height: number;
   children: React.ReactNode;
-  color: string;
+  styles: string;
   emoji: string;
   onClick?: () => void;
 }) => (
   <div
-    className={`col-span-${width} row-span-${height} bg-${color} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
+    className={`${styles} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
     onClick={onClick}
   >
     <p className={`font-semibold text-lg`}>{children}</p>
@@ -25,14 +21,48 @@ const Block = ({
 );
 
 const sections = [
-  { text: "Projects", emoji: "🚀", color: "gray-300", big: true },
-  { text: "Blog", emoji: "📗", color: "green-400" },
-  { text: "Contact", emoji: "📱", color: "teal-400", big: true },
-  { text: "CV", emoji: "👨‍💻", color: "blue-400" },
+  {
+    text: "Projects",
+    emoji: "🚀",
+    blockStyle: "bg-indigo-500 col-span-2 row-span-2",
+    sectionTitleStyle: "text-indigo-500 border-indigo-500",
+    sectionBlocksStyle: "bg-indigo-500",
+  },
+  {
+    text: "Blog",
+    emoji: "📗",
+    blockStyle: "bg-green-400 col-span-1 row-span-1",
+    sectionTitleStyle: "text-green-400 border-green-400",
+    sectionBlocksStyle: "bg-green-400",
+  },
+  {
+    text: "Contact",
+    emoji: "📱",
+    blockStyle: "bg-teal-400 col-span-2 row-span-1",
+    sectionTitleStyle: "text-teal-400 border-teal-400",
+    sectionBlocksStyle: "bg-teal-400",
+  },
+  {
+    text: "CV",
+    emoji: "👨‍💻",
+    blockStyle: "bg-blue-400 col-span-1 row-span-1",
+    sectionTitleStyle: "text-blue-400 border-blue-400",
+    sectionBlocksStyle: "bg-blue-400",
+  },
 ];
 
 export const Sections = () => {
-  const [activeElement, setActiveElement] = useState(0);
+  const sectionRefs = useRef<any[]>([]);
+
+  const [selectedElement, setSelectedElement] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (selectedElement === null) return;
+
+    console.log(selectedElement);
+
+    sectionRefs?.current[selectedElement]?.scrollIntoView();
+  }, [sectionRefs, selectedElement]);
 
   return (
     <>
@@ -40,10 +70,8 @@ export const Sections = () => {
         {sections.map((element, i) => (
           <Block
             key={i}
-            color={element.color}
-            width={element.big || activeElement === i ? 2 : 1}
-            height={activeElement === i ? 2 : 1}
-            onClick={() => setActiveElement(i)}
+            styles={element.blockStyle}
+            onClick={() => setSelectedElement(i)}
             emoji={element.emoji}
           >
             {element.text}
@@ -51,24 +79,24 @@ export const Sections = () => {
         ))}
       </div>
       {sections.map((element, i) => (
-        <div key={i} className={``}>
+        <div key={i} ref={(el) => (sectionRefs.current[i] = el)}>
           <h3
-            className={`text-${element.color} font-bold text-4xl border-b border-${element.color} mb-14`}
+            className={`${element.sectionTitleStyle} font-bold text-4xl border-b mb-14`}
           >
             {element.text}
           </h3>
           <div className="grid grid-rows-2 grid-cols-4 grid-flow-col gap-2 h-[500px] mb-60">
             <div
-              className={`bg-${element.color} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
+              className={`${element.sectionBlocksStyle} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
             ></div>
             <div
-              className={`bg-${element.color} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
+              className={`${element.sectionBlocksStyle} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
             ></div>
             <div
-              className={`bg-${element.color} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
+              className={`${element.sectionBlocksStyle} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
             ></div>
             <div
-              className={`bg-${element.color} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
+              className={`${element.sectionBlocksStyle} rounded-[60px] p-10 flex flex-col-reverse transition-all hover:scale-95 hover:opacity-90`}
             ></div>
           </div>
         </div>
