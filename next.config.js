f974a1aa3833e391b7e93e00cda4ea5814248efe
Sebastián Papanicolau <sebastian.papanicolau@scale.com>
2025-07-12 18:48:13 -0300

    /schedule cal.com

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/schedule",
+        destination:
+          "https://cal.com/sebastian-papanicolau-9mw3mx/30min?overlayCalendar=true",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
